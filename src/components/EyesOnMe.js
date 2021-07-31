8ba48bcbd4002f20313ad3621fc2df5b73a1ebf5
Amal Abdu <abduamal@ymail.com>
2021-07-31 09:54:05 -0500

    commit tree 81f77bfc104d472d86833d4753c5a8d33594a290

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  // constructor() {
+  //
+  // }
+
+  render() {
+    return(
+      <button
+        onFocus={() => console.log('Good!');}
+        onBlur={() => console.log('Hey! Eyes on me!');}>
+      </button>
+    )
+  }
+}
+
+export default EyesOnMe
